33dc74dd88d8ebba961ffcd295b98a66f1cfdea8
tatanmejia <jhotan_22m@hotmail.com>
2017-03-12 16:12:14 -0500

    Array 9 complementando

--- array9_suma_matrices.cpp
+++ array9_suma_matrices.cpp
@@ -2,52 +2,99 @@
 #include <stdlib.h>
 
 void pedirdatos();
-void suma_matriz(int **,int **,int,int);
+void suma_matriz();
+void mostrar_matriz();
 
-int **puntero_mat1, **puntero_mat2,nfilas,ncolumnas;
+int **puntero_mat1, **puntero_mat2, **matriz_suma,nfilas,ncolumnas;
 
 int main(){
 
     pedirdatos();
     suma_matriz();
+    mostrar_matriz();
+    free(matriz_suma);
+
+    for(int i=0;i<nfilas;i++){
+        delete[] puntero_mat2[i];
+    }
+    delete[]puntero_mat2;
 
 
+    for(int i=0;i<nfilas;i++){
+        delete[] puntero_mat1[i];
+    }
+    delete[]puntero_mat1;
 
+return 0;
 }
 
 void pedirdatos(){
+
     printf("Digite el numero de filas: ");
     scanf("%d",&nfilas);
     printf("Digite el numero de columnas: ");
     scanf("%d",&ncolumnas);
 
-    puntero_mat1=new int*[nfilas];
-    for(int i;i<nfilas;i++){
+    puntero_mat1 = (int **)malloc(nfilas * sizeof(int *));
+	for(int x = 0; x < ncolumnas; x++){
+		puntero_mat1[x] = (int *)malloc(ncolumnas * sizeof(int ));
+	}
 
-        puntero_mat1[i]=new int[ncolumnas];
-    }
-    printf("Digite los elementos de la primera matriz: \n");
-    for(int i=0;i<nfilas;i++){
-        for(int j=0;j<ncolumnas;j++){
-            printf("Digite un numero %d",i+1);
-            scanf("%d",*(*(puntero_mat1+i)+j));
-
+    for(int a=0; a<nfilas; a++){
+        for(int b=0; b<ncolumnas; b++){
+            printf("\nIngrese dato para posicion [%d,%d]: ", a, b);
+            scanf("%d", &(*(*(puntero_mat1+b)+a)));
         }
     }
-     puntero_mat2=new int*[nfilas];
-    for(int i;i<nfilas;i++){
 
-        puntero_mat2[i]=new int[ncolumnas];
-    }
-    printf("Digite los elementos de la segunda matriz: \n");
-    for(int i=0;i<nfilas;i++){
-        for(int j=0;j<ncolumnas;j++){
-            printf("Digite un numero %d",i+1);
-            scanf("%d",*(*(puntero_mat2+i)+j));
+
+   puntero_mat2 = (int **)malloc(nfilas * sizeof(int *));
+	for(int x = 0; x < ncolumnas; x++){
+		puntero_mat2[x] = (int *)malloc(ncolumnas * sizeof(int ));
+	}
+
+    for(int a=0; a<nfilas; a++){
+        for(int b=0; b<ncolumnas; b++){
+            printf("\nIngrese dato para posicion [%d,%d]: ", a, b);
+            scanf("%d", &(*(*(puntero_mat2+b)+a)));
 
         }
     }
 }
 
 
-void suma_matriz(int **puntero_mat1, int **puntero_mat2, int nfilas, int ncolumnas)
+void suma_matriz(){
+  matriz_suma = (int **)malloc(nfilas * sizeof(int *));
+	for(int i=0; i<ncolumnas; i++){
+		matriz_suma[i] = (int *)malloc(ncolumnas * sizeof(int ));
+	}
+
+    for(int a=0; a<nfilas; a++){
+        for(int b=0; b<ncolumnas; b++){
+            (*(*(matriz_suma+a)+b))=0;
+            for(int c=0;c<ncolumnas;c++){
+               (*(*(matriz_suma+a)+b))=((*(*(puntero_mat1+a)+c)) + (*(*(puntero_mat2+c)+b)));
+            }
+        }
+    }
+
+
+   /* for(int i=0;i<nfilas;i++){
+        for(int j=0;i<ncolumnas;i++){
+            (*(*(puntero_mat1+i)+j)+= *(*(puntero_mat2+i)+j));
+        }
+    }
+}*/}
+
+void mostrar_matriz(){
+    printf("\nLa suma de las 2 matrices es: \n");
+    for(int i=0;i<nfilas;i++){
+
+        for(int j=0;j<ncolumnas;j++){
+            printf("%d",*(*(matriz_suma+i)+j));
+        }
+        printf("\n");
+    }
+
+
+}
